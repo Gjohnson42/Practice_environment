5303dfc7a644868b6f98dd26cb2a08804dbe2240
Garrett Johnson <gjohnson2718@gmail.com>
2016-01-22 08:56:22 -0500

    Chromebook commit #1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 /**
  * Created by Garrett on 1/21/2016.
  * This is meant to be the primary file to practice random stuff in Java, for the sake of learning it.
- * Make a seperate branch if you aren't sure what you're doing.
+ * Make a seperate branch if you aren't sure what you're doing. This should be a chromebook based change.
+ * I'll check at home whether or not github decided to freak out.
  */
 public class Main {
 }
