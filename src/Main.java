12d56d0f12e6ddee67d903bc0845638856a1bea1
Garrett Johnson <gjohnson2718@gmail.com>
2016-01-27 11:32:34 -0500

    final, minus debugging

--- src/Main.java
+++ src/Main.java
@@ -11,12 +11,16 @@ public class Main {
         String[] monsters= {"red dragon", "mushroom", "chihuaha"};
         String[] actions= {"attacked!", "just ambled by.", "caught you by suprise!"};
 // since you need an int to use the randomizer, convert the array lengths to ints.
-        int a= adjectives.length;
-        int m= monsters.length;
-        int ac= actions.length;
+        int a= (int)adjectives.length;
+        int m= (int)monsters.length;
+        int ac= (int)actions.length;
         //now to create the random numbers, which requires a casting to int. I could just tell it the lengths by hand
         //but that would make my code more brittle.
-
+        int rand1= (int)(Math.random()*a);
+        int rand2= (int)(Math.random()*m);
+        int rand3= (int)(Math.random()*ac);
+        String phrase= ("a "+ adjectives[rand1]+ " "+ monsters[rand2]+ " "+ actions[rand3]);
+        System.out.println(phrase);
 
 
     }
